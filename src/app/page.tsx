d5891a4ec984a570c199d6e02f17fbe4b8447a63
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-03-17 16:16:18 +0700

    2-S Sign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/clerk-react";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* TODO: I want custom button to show dynamically based on signed in/out */}
-      <SignInButton mode="modal" />
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign out</Button>
+        </SignOutButton>
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
