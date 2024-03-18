a8d0e061fdc9bb0dc5ccec20948b42f7de7ae02d
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-03-18 15:48:28 +0700

    3-Q Organization's files

--- convex/files.ts
+++ convex/files.ts
@@ -2,19 +2,23 @@ import { mutation, query } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 
 export const createFile = mutation({
-  args: { name: v.string() },
+  args: { name: v.string(), orgId: v.string() },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) throw new ConvexError("Login to uplaod");
 
-    await ctx.db.insert("files", { name: args.name });
+    await ctx.db.insert("files", { name: args.name, orgId: args.orgId });
   },
 });
 export const getFiles = query({
-  args: {},
+  args: { orgId: v.string() },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) return [];
-    await ctx.db.query("files").collect();
+
+    return await ctx.db
+      .query("files")
+      .withIndex("by_orgId", //FIXME:)
+      .collect();
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,21 @@ import {
   SignOutButton,
   SignedIn,
   SignedOut,
+  useOrganization,
 } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const { organization } = useOrganization();
+  // TODO: We want to query faster and have their own files for each organization
+  // There're 4 locations to fix
+  const files = useQuery(
+    api.files.getFiles
+    // FIXME:
+  );
+  const createFile = useMutation(api.files.createFile);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
@@ -22,6 +34,19 @@ export default function Home() {
           <Button>Sign in</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file) => (
+        <div key={file._id}>{file.name} </div>
+      ))}
+
+      <Button
+        onClick={() => {
+          // FIXME:
+          createFile({ name: "Hello", orgId: organization.id });
+        }}
+      >
+        Create
+      </Button>
     </main>
   );
 }
